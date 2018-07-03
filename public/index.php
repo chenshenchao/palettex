2dfec945542442fa69763cb6e7e2ee71d2189379
chenshenchao <chenshenchao@outlook.com>
2018-07-04 00:37:43 +0800

    entry.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
-<?php
+<?php namespace plattex;
+
+use purplex\Application;
+
 /**
  * Palettex Entry
  * 
  */
-require '../vendor/autoload.php';
+function run($startTime, $projectPath) {
+    require $projectPath.'/vendor/autoload.php';
+    $application = new Application($projectPath);
+    $application->apply($startTime);
+}
+
+run(microtime(true), dirname(__DIR__));
